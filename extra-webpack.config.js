797ae5c2e6087d7cc27f56ab3ccdf1180f0d254f
Nicky Kimaina <nkimaina@ampath.or.ke>
2020-01-07 16:49:53 +0300

    Added ability to fetch form schema, compile it and cache it

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -6,39 +6,45 @@ module.exports = (angularWebpackConfig, options) => {
   const singleSpaWebpackConfig = singleSpaAngularWebpack(angularWebpackConfig, options)
 
   singleSpaWebpackConfig.externals['@openmrs/esm-api'] = '@openmrs/esm-api';
-  // console.log('webpack', singleSpaWebpackConfig.output);
+  // console.log('webpack', singleSpaWebpackConfig);
 
-  const path = singleSpaWebpackConfig.output.path;
-  const publicPath = singleSpaWebpackConfig.output.publicPath;
-  // HACK to fix the deployment of fonts for bootstrap
-  singleSpaWebpackConfig.plugins.push(
-    new ReplaceInFileWebpackPlugin([{
-      dir: path,
-      files: ['main-es2015.js'],
-      rules: [
-        {
-          search: 'glyphicons-halflings-regular.eot',
-          replace: publicPath + 'glyphicons-halflings-regular.eot'
-        },
-        {
-          search: 'glyphicons-halflings-regular.woff',
-          replace: publicPath + 'glyphicons-halflings-regular.woff'
-        },
-        // {
-        //   search: 'glyphicons-halflings-regular.woff2',
-        //   replace: publicPath + 'glyphicons-halflings-regular.woff2'
-        // },
-        {
-          search: 'glyphicons-halflings-regular.ttf',
-          replace: publicPath + 'glyphicons-halflings-regular.ttf'
-        },
-        {
-          search: 'glyphicons-halflings-regular.svg',
-          replace: publicPath + 'glyphicons-halflings-regular.svg'
-        }
-      ]
-    }]
-    ));
+  if (singleSpaWebpackConfig.mode === 'production') {
+    const path = singleSpaWebpackConfig.output.path;
+    const publicPath = singleSpaWebpackConfig.output.publicPath;
+    console.log('path', path);
+    console.log('publicPath', publicPath);
+    // HACK to fix the deployment of fonts for bootstrap
+    // NOTE: this does not work with ng server --prod true, because the file does not exist
+    singleSpaWebpackConfig.plugins.push(
+      new ReplaceInFileWebpackPlugin([{
+        dir: path,
+        files: ['main-es2015.js'],
+        rules: [
+          {
+            search: 'glyphicons-halflings-regular.eot',
+            replace: publicPath + 'glyphicons-halflings-regular.eot'
+          },
+          {
+            search: 'glyphicons-halflings-regular.woff',
+            replace: publicPath + 'glyphicons-halflings-regular.woff'
+          },
+          // {
+          //   search: 'glyphicons-halflings-regular.woff2',
+          //   replace: publicPath + 'glyphicons-halflings-regular.woff2'
+          // },
+          {
+            search: 'glyphicons-halflings-regular.ttf',
+            replace: publicPath + 'glyphicons-halflings-regular.ttf'
+          },
+          {
+            search: 'glyphicons-halflings-regular.svg',
+            replace: publicPath + 'glyphicons-halflings-regular.svg'
+          }
+        ]
+      }]
+      ));
+
+  }
 
   // singleSpaWebpackConfig.module.rules.push({parser: {system: false}})
 
